61bba52e02863c9bb4afb189f30b14fee81fa719
Lydia B <lydiabaik@gmail.com>
2020-01-22 04:25:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 
   def match(word_array)
     anagrams = []
-    word_array.collect do |x|
+    word_array.each do |x|
       if x.split("").sort == @word.split("").sort
         anagrams << x
       end
