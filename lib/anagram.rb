758f6a348011d539a4ac4e01036e63e2c49827e9
Lydia B <lydiabaik@gmail.com>
2020-01-22 04:24:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,9 +11,7 @@ class Anagram
       if x.split("").sort == @word.split("").sort
         anagrams = []
         anagrams << w
-        return anagrams
       end
-        anagrams = []
         return anagrams
       end
   end
