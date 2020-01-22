1d7fdac5aa014d2ad95461ecb5424dbd6ae4170e
Lydia B <lydiabaik@gmail.com>
2020-01-22 04:11:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+  word_list.each do |option|
+
+  new_arr = option.split("")
+
+  if new_arr.sort == @word.sort
+    puts option
+  end
+
+end
+end
+
+end
