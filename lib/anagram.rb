589ee5c83cc540da460324b50168ec9ba4628269
Lydia B <lydiabaik@gmail.com>
2020-01-22 04:45:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,38 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(word_array)
+    anagrams = []
+
+    word_array.each do |x|
+      if x.split("").sort == @word.split("").sort
+        anagrams.push(x)
+      end
+      end
+      
+      return anagrams
+  end
+
+
+end
+# #
+#
+# word = listen
+# elists, google, inlets, banana
+#
+# #
+# it 'should detect an anagram' do
+#   detector = Anagram.new('listen')
+#   listen = detector.match %w(enlists google inlets banana)
+#   expect(listen).to eq(['inlets'])
+#
+#
+# # word = 'ba'
+# # word_array = ['ab', 'abc', 'bac']
+# #
+# # [a,b].sort
